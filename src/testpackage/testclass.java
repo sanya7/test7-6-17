6afe42d5632110e4e8059888ebe3da0c35747748
sanya <sanya@sanya-PC>
2017-06-07 23:06:15 +0530

    2nd commit 11:06

--- src/testpackage/testclass.java
+++ src/testpackage/testclass.java
@@ -4,11 +4,15 @@ public class testclass {
 
 	public String concatenate(String one,String two)
 	{
-		return one + two;
+		return one + two;//refactor variables(ALT +SHIFT + R)
 	}
 	
 	public int multiply(int num1, int num2)
 	{
+		return mulnum(num1, num2);//extract method(ALT + SHIFT + M) --> mulnum is my method name
+	}
+
+	private int mulnum(int num1, int num2) {//refactor > inline (ALT + SHIFT + I)
 		return num1 * num2;
 	}
 }
